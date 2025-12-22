8f02e8b17200657328918f9bf40f905f903e31eb
Laurie Hess <lettergriddle@gmail.com>
2025-12-22 01:27:11 -0500

    Fix confetti start position

--- app/jukebox/page.js
+++ app/jukebox/page.js
@@ -945,7 +945,7 @@ export default function JukeboxGame() {
         {showConfetti && (
           <div className="confetti-container">
             {Array.from({ length: 40 }).map((_, i) => (
-              <div key={i} className="confetti" style={{ left: `${Math.random() * 100}%`, top: `${Math.random() * 50}%`, animationDelay: `${Math.random() * 2}s` }}>
+              <div key={i} className="confetti" style={{ left: `${Math.random() * 100}%`, top: `top: '-30px'`, animationDelay: `${Math.random() * 2}s` }}>
                 {confettiEmojis[i % confettiEmojis.length]}
               </div>
             ))}
