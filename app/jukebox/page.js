8800031277b0c725bb8825ddedbff72b0824938c
Laurie Hess <lettergriddle@gmail.com>
2026-01-06 09:20:19 -0500

    Add promotional lines to Jukebox share text

--- app/jukebox/page.js
+++ app/jukebox/page.js
@@ -360,7 +360,7 @@ export default function JukeboxGame() {
 
   // Share function
   const handleShare = async () => {
-    const shareText = `I completed the ${currentPuzzle.category} track on Letter Griddle Jukebox! 🎵☕🥞\nlettergriddlecafe.com/jukebox`;
+    const shareText = `I completed the ${trackName} track on Letter Griddle Jukebox! 🎵 ☕ 🥞\nlettergriddlecafe.com/jukebox\nFree & ad-free!\nPart of the Letter Griddle Games 🥞\nMore games: lettergriddle.com`;
     
     if (navigator.share) {
       try {
